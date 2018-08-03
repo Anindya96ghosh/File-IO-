c11630e40e50b83f62fc7909bc111c1c6487e3b3
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-03 23:53:00 +0530

    Add files via upload

---
+++ WordCount/com/fileWordCount/FileWordCount.java
@@ -0,0 +1,39 @@
+//to count the words present in the file
+
+package com.fileWordCount;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileWordCount {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		
+		Scanner scanner = new Scanner(System.in);
+		scanner.useDelimiter(" ");
+		System.out.print("Enter input file path and name:");
+		String inFile = scanner.nextLine();
+		System.out.println("You entered: " + inFile);
+		//FileInputStream fs = new FileInputStream(inFile); 
+		File file = new File(inFile);
+		
+		
+		
+		try(Scanner sc=new Scanner(file))
+		{
+			int count=0;
+			while(sc.hasNext())
+			{
+				sc.next();
+				count++;
+			}
+			System.out.println("Number of words: "+count);
+		}
+		
+		
+	}
+
+}
