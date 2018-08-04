8d97bb4e893a3114582c551f6766f8d26b91f92a
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 12:56:27 +0530

    Add files via upload

---
+++ JavaGrep/com/java/grep/JavaGrep.java
@@ -0,0 +1,41 @@
+package com.java.grep;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class JavaGrep {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter input file path and name:");
+		String inFile = scanner.nextLine();
+		System.out.println("You entered: " + inFile);
+		//FileInputStream fs = new FileInputStream(inFile); 
+		File file = new File(inFile);
+		System.out.print("Enter string you want to search:");
+		String string = scanner.nextLine();
+		int lineCount=0;
+		int flag=0;
+		
+		BufferedReader br=new BufferedReader(new FileReader(inFile));
+		String line;
+		while((line=br.readLine())!=null)
+		{
+			
+			lineCount++;
+			if(line.contains(string))
+			{
+				flag = 1;
+				System.out.println("Line number "+lineCount+" Line: "+line);
+			}
+		}
+		if(flag!=1)
+			System.out.println("the string is not present in the file ");
+				
+		
+	}
+
+}
